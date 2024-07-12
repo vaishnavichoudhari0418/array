aa27c0f8e0a3eb09266736de3301d8c6a8b3a87c
vaishnavichoudhari119@gmail.com <vaishnavichoudhari119@gmail.com>
2024-07-12 20:09:14 +0530

    example of keyof method

---
+++ hello.js
@@ -0,0 +1,10 @@
+const students=["Vaishnavi","Paridhi","Sakshi","Anshul","Nayan","Sushrut"];
+for(let i=0;i<students.length;i++)
+{
+    console.log(`Hello ${students[i]}`)
+}
+//key of
+for(const stu of students)
+    {
+        console.log(`Hello ${stu}`)
+    }
